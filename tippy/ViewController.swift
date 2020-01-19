5e2e6405a80bb85cd22d0945c48a30bce273b982
subram43 <subram43@purdue.edu>
2020-01-19 18:33:25 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Aditya Subramaniam on 1/19/20.
+//  Copyright © 2020 subram43. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
