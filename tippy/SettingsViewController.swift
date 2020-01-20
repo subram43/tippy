27c7f1091ee49fb2eee66c1295378a1e683ac137
subram43 <subram43@purdue.edu>
2020-01-20 01:35:33 -0500

    added dark mode and settings

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Aditya Subramaniam on 1/19/20.
+//  Copyright © 2020 subram43. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var percentage1Field: UITextField!
+    @IBOutlet weak var percentage2Field: UITextField!
+    @IBOutlet weak var percentage3Field: UITextField!
+    @IBOutlet weak var darkmodeSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        // Do any additional setup after loading the view.
+        percentage1Field.text = defaults.string(forKey: "percentage1") ?? "15"
+        percentage2Field.text = defaults.string(forKey: "percentage2") ?? "18"
+        percentage3Field.text = defaults.string(forKey: "percentage3") ?? "20"
+        
+        if (defaults.bool(forKey: "darkMode")) {
+            overrideUserInterfaceStyle = .dark
+            darkmodeSwitch.isOn = true
+        } else {
+            overrideUserInterfaceStyle = .light
+            darkmodeSwitch.isOn = false
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func changeDefaultPercentages(_ sender: Any) {
+        let defaults = UserDefaults.standard;
+        defaults.set(percentage1Field.text, forKey: "percentage1")
+        defaults.set(percentage2Field.text, forKey: "percentage2")
+        defaults.set(percentage3Field.text, forKey: "percentage3")
+        defaults.synchronize()
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func darkmodeChanged(_ sender: Any) {
+        let defaults = UserDefaults.standard;
+        
+        if (overrideUserInterfaceStyle == .dark) {
+            overrideUserInterfaceStyle = .light
+            defaults.set(false, forKey: "darkMode")
+        } else {
+            overrideUserInterfaceStyle = .dark
+            defaults.set(true, forKey: "darkMode")
+        }
+        
+        defaults.synchronize()
+    }
+}
--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -14,13 +14,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    
+    var tipPercentages: [Double]! = [0.15, 0.18, 0.2]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let defaults = UserDefaults.standard
+        
+        if (defaults.bool(forKey: "darkMode")) {
+            overrideUserInterfaceStyle = .dark
+        } else {
+            overrideUserInterfaceStyle = .light
+        }
+        
+        let percentage1 = defaults.string(forKey: "percentage1") ?? "15"
+        let percentage2 = defaults.string(forKey: "percentage2") ?? "18"
+        let percentage3 = defaults.string(forKey: "percentage3") ?? "20"
+        
+        tipPercentages[0] = Double(percentage1)! / 100.0;
+        tipPercentages[1] = Double(percentage2)! / 100.0;
+        tipPercentages[2] = Double(percentage3)! / 100.0;
+        
+        tipControl.setTitle("\(percentage1)%", forSegmentAt: 0)
+        tipControl.setTitle("\(percentage2)%", forSegmentAt: 1)
+        tipControl.setTitle("\(percentage3)%", forSegmentAt: 2)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Update changes
+        let defaults = UserDefaults.standard
+        
+        if (defaults.bool(forKey: "darkMode")) {
+            overrideUserInterfaceStyle = .dark
+        } else {
+            overrideUserInterfaceStyle = .light
+        }
+        
+        let percentage1 = defaults.string(forKey: "percentage1") ?? "15"
+        let percentage2 = defaults.string(forKey: "percentage2") ?? "18"
+        let percentage3 = defaults.string(forKey: "percentage3") ?? "20"
+        
+        tipPercentages[0] = Double(percentage1)! / 100.0;
+        tipPercentages[1] = Double(percentage2)! / 100.0;
+        tipPercentages[2] = Double(percentage3)! / 100.0;
+        
+        tipControl.setTitle("\(percentage1)%", forSegmentAt: 0)
+        tipControl.setTitle("\(percentage2)%", forSegmentAt: 1)
+        tipControl.setTitle("\(percentage3)%", forSegmentAt: 2)
+    }
 
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
@@ -32,7 +76,7 @@ class ViewController: UIViewController {
         let bill = Double(billField.text!) ?? 0
         
         // Calculate the tip and the total
-        let tipPercentages = [0.15, 0.18, 0.2]
+//        let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
             
